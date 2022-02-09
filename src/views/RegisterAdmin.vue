58cb2f5488d92923bcd704120c0287f0f8ef2006
grandorder999 <suzuo.kawabata@rakus-partners.co.jp>
2022-02-09 14:11:54 +0900

    表示内容変更のため、RegisterAdmin.vueを編集

--- src/views/RegisterAdmin.vue
+++ src/views/RegisterAdmin.vue
@@ -1,20 +1,5 @@
 <template>
   <div class="sample">
-    <header>
-      <div class="container">
-        <div class="header">
-          <div class="header-left">
-            <a href="login.html">
-              <img class="logo" src="img/header_logo.png" />
-            </a>
-          </div>
-
-          <div class="header-right">
-            <a href="registerAdmin.html">管理者登録</a>
-          </div>
-        </div>
-      </div>
-    </header>
     <div class="top-wrapper">
       <div class="container">
         <div class="row register-page">
