510cf3dc095523dba7dd0be029597703872dfb23
grandorder999 <suzuo.kawabata@rakus-partners.co.jp>
2022-02-09 16:06:22 +0900

    機能追加のため、画面遷移を設定するファイルを編集

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,11 @@ Vue.use(VueRouter);
 const routes: Array<RouteConfig> = [
   // // パスが登録されていない場合はログイン画面へ(LoginAdmin.vueを作成したらコメントを外してください)
   {
+    path: "/employeeDetail/:id",
+    component: () => import("../views/EmployeeDetail.vue"),
+  },
+
+  {
     path: "/employeeList",
     component: () => import("../views/EmployeeList.vue"),
   },
