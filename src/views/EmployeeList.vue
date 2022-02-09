68945732fc07547dbd72d46c13fdc57ef7a11385
grandorder999 <suzuo.kawabata@rakus-partners.co.jp>
2022-02-09 15:20:16 +0900

    機能追加のため、EmployeeList.vueを編集

--- src/views/EmployeeList.vue
+++ src/views/EmployeeList.vue
@@ -4,8 +4,28 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { Employee } from "@/types/employee";
 @Component
-export default class XXXComponent extends Vue {}
+/**
+ * Vuexストアのアクション経由で非同期でWebAPIから従業員一覧を取得する.
+ */
+export default class EmployeeList extends Vue {
+  created(): void {
+    this.$store.dispatch("getEmployeeList");
+  }
+  /**
+   * 非同期で取得したVuexストア内の従業員数を取得し返す
+   */
+  get employeeCount(): number {
+    return this.$store.getters.getEmployeeCount;
+  }
+  /**
+   * 非同期で取得したVuexストア内の従業員一覧を取得し返す
+   */
+  get employees(): Array<Employee> {
+    return this.$store.getters.getEmployees;
+  }
+}
 </script>
 
 <style scoped></style>
