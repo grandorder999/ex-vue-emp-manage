2c1da7bc45cb7851426ce524e72cb91867084765
grandorder999 <suzuo.kawabata@rakus-partners.co.jp>
2022-02-09 14:26:20 +0900

    機能追加のため、LoginAdmin.vueを編集

--- src/views/LoginAdmin.vue
+++ src/views/LoginAdmin.vue
@@ -1,5 +1,62 @@
 <template>
-  <div></div>
+  <div class="sample">
+    <div class="top-wrapper">
+      <div class="container">
+        <div class="row login-page">
+          <div class="col s12 z-depth-6 card-panel">
+            <div class="error">{{ errorMessage }}</div>
+            <form class="login-form">
+              <div class="row"></div>
+              <div class="row">
+                <div class="input-field col s12">
+                  <i class="material-icons prefix">mail_outline</i>
+                  <input
+                    v-model="mailAddress"
+                    class="validate"
+                    id="mailAddress"
+                    type="email"
+                  />
+                  <label
+                    for="mailAddress"
+                    data-error="wrong"
+                    data-success="right"
+                    >メールアドレス</label
+                  >
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s12">
+                  <i class="material-icons prefix">lock_outline</i>
+                  <input v-model="password" id="password" type="password" />
+                  <label for="password">パスワード</label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s12">
+                  <button
+                    type="button"
+                    v-on:click="loginAdmin"
+                    class="btn btn-register waves-effect waves-light col s12"
+                  >
+                    ログイン
+                  </button>
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s6 m6 l6">
+                  <p class="margin medium-small">
+                    <router-link to="/registerAdmin"
+                      >管理者登録はこちら</router-link
+                    >
+                  </p>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
